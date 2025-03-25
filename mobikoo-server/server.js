bd0176ea1650040e3e595198914bfc82ec0806fb
Ansh Agarwal <anshagarwal.rishikesh@gmail.com>
2025-03-26 03:43:22 +0530

    chnage

--- mobikoo-server/server.js
+++ mobikoo-server/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-    origin: "http://mobikoo.com", // Change this to match your frontend
+    origin: "https://mobikoo.com", // Change this to match your frontend
     methods: "GET,POST,PUT,DELETE",
     credentials: true
   }));
