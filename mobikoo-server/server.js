24580489dec4aec4ebccc0d549ae36a48bb65ac5
Ansh Agarwal <anshagarwal.rishikesh@gmail.com>
2025-03-26 03:16:14 +0530

    commit

--- mobikoo-server/server.js
+++ mobikoo-server/server.js
@@ -20,7 +20,11 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(cors({
+    origin: "http://mobikoo.com", // Change this to match your frontend
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  }));
 // Connect to Database
 connectDB();
 
